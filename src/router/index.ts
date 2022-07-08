9ec3b78d4405fff3d21711c13e1e3a8485f277ec
lllem <s.m.shemiakin@gmail.com>
2022-07-08 15:04:25 +0300

    Implemented loading and displaying notes

--- src/router/index.ts
+++ src/router/index.ts
@@ -8,12 +8,10 @@ const routes: Array<RouteRecordRaw> = [
     component: HomeView,
   },
   {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue'),
+    path: '/notes/:id',
+    name: 'note',
+    props: true,
+    component: () => import('../views/NoteView.vue'),
   },
 ];
 
